f186b097ae4e6a1c992f8cdd1f9adf78305b57fa
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-04-30 17:06:15 +0200

    Add message to AllComponentsMustBeValidException.

--- src/Domain/Validator/AllComponentsMustBeValidException.php
+++ src/Domain/Validator/AllComponentsMustBeValidException.php
@@ -3,7 +3,14 @@
 namespace App\Domain\Validator;
 
 
+use Throwable;
+
 class AllComponentsMustBeValidException extends \Exception
 {
+    public function __construct(string $message = "", int $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->message = 'All components must be valid.';
+    }
 
 }
