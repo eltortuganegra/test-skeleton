1b4715156b7ec7fde0f30bc6467b85f00b5095a8
eltortuganegra <jorge.sanchez@eltortuganegra.com>
2018-05-03 18:11:48 +0200

    Refactor PublishAdController.

--- src/Controller/PublishAdController.php
+++ src/Controller/PublishAdController.php
@@ -24,40 +24,26 @@ class PublishAdController extends Controller
     public function publish(Request $resquest)
     {
         try {
-            $serviceResponse = $this->executePublishAdService($resquest);
-            $ad = $serviceResponse->getAd();
-            if ($ad->getStatus()->getValue() == AdStatusFactory::AD_STATUS_PUBLISHED) {
-                $output = json_encode([
-                    'message' => 'ok'
-                ]);
-
-                return new Response($output);
-            } else {
-                $output = json_encode([
-                    'message' => 'fail'
-                ]);
-                $response = new Response($output);
-                $response->setStatusCode(400);
-
-                return $response;
-            }
+            $response = $this->executePublishAdService($resquest);
         } catch(\Exception $exception) {
             $response = $this->getResponseForException($exception);
 
             return $response;
         }
+
+        return $response;
     }
 
-    private function executePublishAdService(Request $resquest): ServiceResponse
+    private function executePublishAdService(Request $resquest): Response
     {
-        $data = json_decode($resquest->getContent(), true);
-        $this->entityManager = $this->getDoctrine()->getManager();
-        $this->adRepository = AdRepositoryFactory::createDoctrine($this->entityManager);
-        $serviceRequest = new PublishAdServiceRequest($data['AdId']);
+        $this->loadAdRepository();
+        $adId = $this->getAdFromRequest($resquest);
+        $serviceRequest = new PublishAdServiceRequest($adId);
         $service = new PublishAdService($this->adRepository);
         $serviceResponse = $service->execute($serviceRequest);
+        $ad = $serviceResponse->getAd();
 
-        return $serviceResponse;
+        return $this->getResponse($ad);
     }
 
     private function getResponseForException($exception): Response
@@ -68,6 +54,59 @@ class PublishAdController extends Controller
         $response = new Response(json_encode($output));
         $response->setStatusCode(400);
 
+        return $response;
+    }
+
+    private function loadAdRepository(): void
+    {
+        $this->entityManager = $this->getDoctrine()->getManager();
+        $this->adRepository = AdRepositoryFactory::createDoctrine($this->entityManager);
+    }
+
+    private function getAdFromRequest(Request $resquest): int
+    {
+        $data = json_decode($resquest->getContent(), true);
+        $adId = $data['AdId'];
+
+        return $adId;
+    }
+
+    private function getResponse($ad): Response
+    {
+        if ( ! $this->isStatusPublished($ad)) {
+            $response = $this->get404Response();
+
+            return $response;
+        }
+
+        $response = $this->getOkResponse();
+
+        return $response;
+    }
+
+    private function isStatusPublished($ad): bool
+    {
+        return $ad->getStatus()->getValue() == AdStatusFactory::AD_STATUS_PUBLISHED;
+    }
+
+    private function get404Response(): Response
+    {
+        $output = json_encode([
+            'message' => 'fail'
+        ]);
+        $response = new Response($output);
+        $response->setStatusCode(400);
+
+        return $response;
+    }
+
+    private function getOkResponse(): Response
+    {
+        $output = json_encode([
+            'message' => 'ok'
+        ]);
+
+        $response = new Response($output);
         return $response;
     }
 }
